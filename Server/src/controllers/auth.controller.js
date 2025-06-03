ade41bd8ed9922469f7971f94d5cde24def77f42
ernest kenneth <ernestkennethyoungsumarno@gmail.com>
2025-06-03 22:53:48 +0100

    Signup finished

--- Server/src/controllers/auth.controller.js
+++ Server/src/controllers/auth.controller.js
@@ -1,9 +1,59 @@
-export const signup  = (req, res) => {
-    res.send("Signup route");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+export const signup = async (req, res) => {
+    // variables we're requesting
+    const { fullName, email, password } = req.body
+    try {
+        // hash password
+        if (!email || !fullName || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters" });
+        }
+        const user = await User.findOne({ email })
+
+        if (user) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        })
+        if (newUser) {
+            // Generate JWT token
+            generateToken(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            }
+            )
+
+        } else {
+            return res.status(400).json({ message: "Invalid user data" });
+        }
+    }
+
+
+    catch (error) {
+        console.log("Error in signup controller", error.message);
+        return res.status(50).json({ message: "Internal error!" });
+
+    }
 };
-export const login  = (req, res) => {
+export const login = (req, res) => {
     res.send("login route");
 };
-export const logout  = (req, res) => {
+export const logout = (req, res) => {
     res.send("logout route");
 };
--- readme.md
+++ readme.md
@@ -12,14 +12,38 @@ A. backend setup
         "mongoose": "^8.15.1",
         "socket.io": "^4.8.1"
     3. create entry file "index.js"
-B. Creating the authentication:
-    1. Create some folders:
-        controllers
-        lib
-        models
-        routes
-        src
-    2. create login logout and signup routes, and test them. 
-    3. seperate the logic into the "controllers" folder. This simplifies the code readabliity. 
+    4. Creating the authentication folder:
+        1. Create some folders:
+            controllers
+            lib
+            models
+            routes
+            src
+        2. create login logout and signup routes, and test them. 
+        3. seperate the logic into the "controllers" folder. This simplifies the code readabliity. 
 
 <!-- Up until this point is 14:22 -->
+
+
+B. Database setup (mongoDB)
+    1. login MongoDB
+    2. create new project --> create new cluster
+    3. create .env file in ROOT folder
+    4. back to mongoDB, find connection setup and see ur password there, grab it and put it in the .env file:
+        MONGODB_URI=mongodb+srv://ernestkennethyoungsumarno:KQ5mVq2QtNnh8bhL@cluster0.jggx2mv.mongodb.net/chat_db?retryWrites=true&w=majority&appName=Cluster0
+
+        PORT=5001
+    5. create db.js in lib and then do dis:
+        
+        import mongoose from "mongoose";
+
+            export const connectDB = async () => {
+                try {
+                    const conn = await mongoose.connect(process.env.MONGODB_URI);
+                    console.log(`MongoDB connected: ${conn.connection.host}`);
+                } catch (error) {
+                    console.log("MongoDB connection error:", error);
+                }
+            }
+    6. then go to entry file(index.js) and import and call the function connectDB();
+    7. 
