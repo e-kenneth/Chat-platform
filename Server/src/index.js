312419cb2a1d76324fb6e20cbebe4a889c0b82aa
ernest kenneth <ernestkennethyoungsumarno@gmail.com>
2025-06-14 09:54:21 +0100

    configuring for production mode

--- Server/src/index.js
+++ Server/src/index.js
@@ -6,11 +6,12 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import { app, server } from "./lib/socket.js";
-
+import path from "path"
 dotenv.config();
 
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 app.use(cors({
   origin: 'http://localhost:5173', // allow your frontend
   credentials: true // if using cookies or sessions
@@ -19,6 +20,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Client", "dist", "index.html"));
+  })
+}
 
 
 server.listen(PORT, () => {
